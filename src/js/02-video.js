169fc028884b7690e08b2cf388cf5d4d9a07a4ff
kolesnva <kolesnychenko_v@icloud.com>
2022-06-09 12:53:16 +0300

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from "@vimeo/player";
 
+let throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_TIME = "videoplayer-current-time";
+const currentTime = getTimeFromStorage();
+
+window.addEventListener('DOMContentLoaded', onPageLoad);
+
+setPlayerStartTime();
+
+function setPlayerStartTime() {
+  if (currentTime) {
+    player.setCurrentTime(currentTime.seconds);
+  };
+}
+
+function onPageLoad() {
+  player.on('timeupdate', throttle(setTimeToStorage, 1000))
+};
+
+function getTimeFromStorage() {
+  return JSON.parse(localStorage.getItem(STORAGE_TIME));
+};
+
+function setTimeToStorage(time) {
+  try {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
+  } catch (error) {
+    console.log(error);
+  }
+};
